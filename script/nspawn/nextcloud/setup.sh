b5dd4f59e8e8bb12f5558423c0cf24267c29ab3d
trwnh <a@trwnh.com>
2022-03-28 00:53:12 -0500

    Update setup.sh

--- script/nspawn/nextcloud/setup.sh
+++ script/nspawn/nextcloud/setup.sh
@@ -15,6 +15,9 @@ mysql -u root -p
 #### create user 'nextcloud'@'localhost' identified by 'password';
 #### grant all privileges on nextcloud.* to 'nextcloud'@'localhost';
 #### flush privileges;
+nano /etc/my.cnf.d/server.cnf
+#### [mysqld]
+#### skip-networking
 systemctl restart mariadb
 
 ## Redis
@@ -27,6 +30,41 @@ usermod -aG redis nextcloud
 systemctl enable --now redis
 
 ## PHP
+nano /etc/php/php.ini
+#### date.timezone = America/Chicago
+#### extension={bcmath,bz2,exif,gd,iconv,imap,intl,imagick,pdo_mysql}
+#### memory_limit = 512M or higher
+#### zend_extension=opcache
+####
+#### [opcache]
+#### opcache.enable = 1
+#### opcache.interned_strings_buffer = 8
+#### opcache.max_accelerated_files = 10000
+#### opcache.memory_consumption = 128
+#### opcache.save_comments = 1
+#### opcache.revalidate_freq = 1
+
+## Nextcloud
+nano /etc/webapps/nextcloud/config/config.php
+# 'trusted_domains' =>
+#   array (
+#     0 => 'localhost',
+#     1 => 'cloud.trwnh.com',
+#   ),    
+# 'overwrite.cli.url' => 'https://cloud.trwnh.com/',
+# 'htaccess.RewriteBase' => '/',
+
+install --owner=nextcloud --group=nextcloud --mode=700 -d /var/lib/nextcloud/sessions
+localectl set-locale en_US.UTF-8
+occ maintenance:install \
+    --database=mysql \
+    --database-name=nextcloud \
+    --database-host=localhost:/run/mysqld/mysqld.sock \
+    --database-user=nextcloud \
+    --database-pass= \
+    --admin-pass= \
+    --admin-email= \
+    --data-dir=/var/lib/nextcloud/data
 
 ## NGINX
 
