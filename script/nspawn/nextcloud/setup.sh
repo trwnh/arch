12fbd8abf457e30737d173dc6abb0893f5851cde
trwnh <a@trwnh.com>
2022-03-28 17:26:49 -0500

    Update setup.sh

--- script/nspawn/nextcloud/setup.sh
+++ script/nspawn/nextcloud/setup.sh
@@ -59,11 +59,11 @@ nano /etc/php/php-fpm.ini
 #
 # [opcache]
 # opcache.enable = 1
+# opcache.memory_consumption = 128
 # opcache.interned_strings_buffer = 8
 # opcache.max_accelerated_files = 10000
-# opcache.memory_consumption = 128
-# opcache.save_comments = 1
 # opcache.revalidate_freq = 1
+# opcache.save_comments = 1
 
 mkdir -p /var/log/php-fpm/access
 curl https://raw.githubusercontent.com/trwnh/arch/main/script/nspawn/nextcloud/php-fpm.pool > /etc/php/php-fpm.d/nextcloud.conf
