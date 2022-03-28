21465ff92e002bd50e7f2aec7e18ca9478697264
trwnh <a@trwnh.com>
2022-03-28 00:17:40 -0500

    Update setup.sh

--- script/nspawn/nextcloud/setup.sh
+++ script/nspawn/nextcloud/setup.sh
@@ -6,6 +6,19 @@
 
 # systemd-nspawn -b -D /var/lib/machines/nextcloud
 
-systemctl enable {nginx,mariadb,redis}
+## MariaDB
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+systemctl enable --now mariadb
+mysql_secure_installation
+mysql -u root -p
+#### create database nextcloud;
+#### create user 'nextcloud'@'localhost' identified by 'password';
+#### grant all privileges on nextcloud.* to 'nextcloud'@'localhost';
+#### flush privileges;
+
+## Redis
+systemctl enable --now redis
+
+systemctl enable {nginxredis}
 
 # CTRL + ]]]
